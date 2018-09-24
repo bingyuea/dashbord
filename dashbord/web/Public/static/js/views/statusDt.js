c7f7a857acfb3274e37cbc94155cae25d405f7bf
AmySay <2387062852@qq.com>
2018-09-24 09:33:40 +0800

    uuuu

--- dashbord/web/Public/static/js/views/statusDt.js
+++ dashbord/web/Public/static/js/views/statusDt.js
@@ -79,70 +79,63 @@ class StatusDt extends BaseView {
             <div className='page-statusDt page-dashboard page'>
                 <div className='left '>
                     <h4 className='label '>二次回路信息</h4>
-                    <div className='dtItem '>
-                        <div className=' flex-layout'>
+                    <div className='topTable'>
+                        <div className=' flex-layout row'>
                             <h6 className='h6 flex'>省份</h6>
                             <h6 className='h6 flex'>城市</h6>
                         </div>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <div className=' font flex'> 湖北</div>
                             <div className='font flex'>湖北</div>
                         </div>
-                    </div>
-                    <div className='dtItem '>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <h6 className='h6 flex'>户名</h6>
                             <h6 className='h6 flex ellipsis'>巡检仪资产编号</h6>
                         </div>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <div className=' font flex'> </div>
                             <div className='font flex'></div>
                         </div>
-                    </div>
-                    <div className='dtItem '>
-                        <div className=' flex-layout'>  
+                        <div className=' flex-layout row'>
                             <h6 className='h6 flex'>用电类别</h6>
                             <h6 className='h6 flex ellipsis'>行业类别</h6>
                         </div>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <div className=' font flex'> </div>
                             <div className='font flex'></div>
                         </div>
-                    </div>
-                    <div className='dtItem '>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <h6 className='h6 flex'>综合倍率</h6>
                             <h6 className='h6 flex ellipsis'>接线方式</h6>
                         </div>
-                        <div className=' flex-layout'>
+                        <div className=' flex-layout row'>
                             <div className=' font flex'> </div>
                             <div className='font flex'></div>
                         </div>
                     </div>
-
                     <h4 className='label2 '>影响二次回路事件</h4>
-                    <div className='tabel'>
-                        <div className='row1 flex-layout'>
-                            <h6 className='h6 flex '>电流失流</h6>
-                            <h6 className='h6 flex'>2018-01-02</h6>
+                    <div className='table'>
+                        <div className='row1 flex-layout row'>
+                            <div className='flex '>电流失流</div>
+                            <div className='flex'>2018-01-02</div>
                         </div>
-                        <div className='row2 flex-layout'>
+                        <div className='row2 flex-layout row'>
                             <div className='flex'> </div>
                             <div className='flex'></div>
                         </div>
-                        <div className='row1 flex-layout'>
+                        <div className='row1 flex-layout row'>
                             <div className='flex'> </div>
                             <div className='flex'></div>
                         </div>
-                        <div className='row2 flex-layout'>
+                        <div className='row2 flex-layout row'>
                             <div className='flex'> </div>
                             <div className='flex'></div>
                         </div>
-                        <div className='row1 flex-layout'>
+                        <div className='row1 flex-layout row'>
                             <div className='flex'> </div>
                             <div className='flex'></div>
                         </div>
-                        <div className='row2 flex-layout'>
+                        <div className='row2 flex-layout row'>
                             <div className='flex'> </div>
                             <div className='flex'></div>
                         </div>
@@ -150,8 +143,35 @@ class StatusDt extends BaseView {
                 </div>
                 {this.renderPageCenter()}
                 <div className='right'>
-                <div >当前状态</div>
-                <div >状态变化</div>
+                    <h4 className='label '>二次回路状态</h4>
+                    <div className='top'>
+                        <div className='title' >当前状态</div>
+                        <div className='dt'>
+                            <div className='title2 '>评分</div>
+                            <div className='mr_b flex-layout'>
+                                <div style={{ fontSize: '20px' }}>98.8</div>
+                                <div className='pa_l flex-layout'>
+                                    <div className='iconfont icon-icon-dsj'></div>
+                                    <span>0.24</span>
+                                </div>
+                            </div>
+                            <div className='title2 '>排名</div>
+                            <div className='mr_b flex-layout'>
+                                <div>
+                                    <span style={{ fontSize: '20px' }}>2</span>
+                                    <span style={{ color: '#5fa3ac' }}>%</span>
+                                </div>
+                                <div className='pa_l flex-layout'>
+                                    <div className='iconfont icon-icon-dsj'></div>
+                                    <span>0.24</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='bottom'>
+                        <div className='title'>状态变化</div>
+
+                    </div>
                 </div>
             </div>
         )
