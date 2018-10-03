c6d5a97bab6db4fa959c8ae2816379b4dee88ea7
songfangyuan <songfangyuan@zto.cn>
2018-10-04 06:10:55 +0800

    ok ,展示有问题，联调完毕

--- dashbord/web/Public/static/js/views/secondaryAnaly.js
+++ dashbord/web/Public/static/js/views/secondaryAnaly.js
@@ -249,12 +249,12 @@ class SecondaryAnaly extends Component {
     if (nameList.length > 0) {
       let arr = []
       nameList.forEach((ele, index) => {
+        let temp = {}
         sourceData.map(item => {
-          let temp = {}
           temp[item.period] = item[ele]
-          temp['name'] = `${item[ele]}`
-          arr.push(temp)
+          temp['name'] = `${ele}`
         })
+        arr.push(temp)
       })
       return arr
     }
@@ -334,30 +334,31 @@ class SecondaryAnaly extends Component {
     }
 
     theme = {
-      data: [
-        {
-          name: 'London',
-          'Jan.': 18.9,
-          'Feb.': 28.8,
-          'Mar.': 39.3,
-          'Apr.': 81.4,
-          May: 47,
-          'Jun.': 20.3,
-          'Jul.': 24,
-          'Aug.': 35.6
-        },
-        {
-          name: 'Berlin',
-          'Jan.': 12.4,
-          'Feb.': 23.2,
-          'Mar.': 34.5,
-          'Apr.': 99.7,
-          May: 52.6,
-          'Jun.': 35.5,
-          'Jul.': 37.4,
-          'Aug.': 42.4
-        }
-      ],
+      // data: [
+      //   {
+      //     name: 'London',
+      //     'Jan.': 18.9,
+      //     'Feb.': 28.8,
+      //     'Mar.': 39.3,
+      //     'Apr.': 81.4,
+      //     May: 47,
+      //     'Jun.': 20.3,
+      //     'Jul.': 24,
+      //     'Aug.': 35.6
+      //   },
+      //   {
+      //     name: 'Berlin',
+      //     'Jan.': 12.4,
+      //     'Feb.': 23.2,
+      //     'Mar.': 34.5,
+      //     'Apr.': 99.7,
+      //     May: 52.6,
+      //     'Jun.': 35.5,
+      //     'Jul.': 37.4,
+      //     'Aug.': 42.4
+      //   }
+      // ],
+      data: data,
       fields: fieldsList,
       keyName: '时间',
       value: '事件数量',
